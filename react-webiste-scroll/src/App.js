2c41512025d75bf9f45d793afae4d0b2c63e8710
schednnedn <scherfnedaei@gmail.com>
2021-12-07 14:14:46 +0100

    secondcommit

--- react-webiste-scroll/src/App.js
+++ react-webiste-scroll/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./App.css";
+import "../src/App.css";
 import { BrowserRouter as Router } from "react-router-dom";
 import Home from "./pages";
 
--- react-webiste-scroll/src/components/HeroSection/HeroElements.js
+++ react-webiste-scroll/src/components/HeroSection/HeroElements.js
@@ -20,8 +20,8 @@ export const HeroContainer = styled.div`
     bottom: 0;
     background: linear-gradient(
         180deg,
-        rgba(0, 0, 0, 0, 0.2) 0%,
-        rgba(0, 0, 0, 0, 0.6) 100%
+        rgba(0, 0, 0, 0.2) 0%,
+        rgba(0, 0, 0, 0.6) 100%
       ),
       linear-gradient(180deg, rgba(0, 0, 0, 0.2) 0%, transparent 100%);
     z-index: 2;
